5328adee3492379c531329af4d62f811a677b810
joacoiannuzzi <joaquin.iannuzzi@ing.austral.edu.ar>
2021-05-07 20:37:03 -0300

    added bootstrap

--- src/routes/createProperty/CreateProperty.tsx
+++ src/routes/createProperty/CreateProperty.tsx
@@ -1,4 +1,5 @@
-import { useForm } from "react-hook-form";
+import { Form } from "react-bootstrap";
+import { Controller, useForm } from "react-hook-form";
 
 import styles from "./CreateProperty.module.scss";
 
@@ -16,85 +17,108 @@ interface FormData {
 }
 
 export function CreateProperty() {
-  const { handleSubmit, register } = useForm<FormData>();
+  const { handleSubmit, register, control } = useForm<FormData>();
 
   const onSubmit = handleSubmit((data) => {
     console.log(data);
   });
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        className={styles.textInput}
-        type="number"
-        id="price"
-        placeholder="Precio"
-        {...register("price")}
-      />
+    <div>
+      <h1>Publicar una propiedad</h1>
+      <Form onSubmit={onSubmit}>
+        {/* <Controller control={control} name="price" 
 
-      <label htmlFor="condition">Condicion:</label>
-      <select id="conditions" {...register("condition")}>
-        <option value="sale">Venta</option>
-        <option value="rental">Alquiler</option>
-      </select>
+      
+      render={({ field }) =>  (
+      
+       } 
+        /> */}
 
-      <input
-        type="text"
-        id="adress"
-        placeholder="Direccion"
-        {...register("address")}
-      />
+        <Controller
+          render={({ field }) => (
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="text" placeholder="price" {...field} />
+            </Form.Group>
+          )}
+          name="price"
+          control={control}
+          defaultValue=""
+        />
 
-      <input
-        type="number"
-        id="squareMeters"
-        placeholder="Metros cuadrados"
-        {...register("squareMeters")}
-      />
+        {/* <input
+          className={styles.textInput}
+          type="number"
+          id="price"
+          placeholder="Precio"
+          {...register("price")}
+        /> */}
 
-      <input
-        type="date"
-        id="constructionDate"
-        placeholder="Fecha de construccion"
-        {...register("constructionDate")}
-      />
+        <label htmlFor="condition">Condicion:</label>
+        <select id="conditions" {...register("condition")}>
+          <option value="sale">Venta</option>
+          <option value="rental">Alquiler</option>
+        </select>
 
-      <input
-        type="text"
-        id="style"
-        placeholder="Estilo"
-        {...register("style")}
-      />
+        <input
+          type="text"
+          id="adress"
+          placeholder="Direccion"
+          {...register("address")}
+        />
 
-      <input
-        type="number"
-        id="ambientsQuantity"
-        placeholder="Cantidad de ambientes"
-        {...register("ambientsQuantity")}
-      />
+        <input
+          type="number"
+          id="squareMeters"
+          placeholder="Metros cuadrados"
+          {...register("squareMeters")}
+        />
 
-      <input
-        type="number"
-        id="roomsQuantity"
-        placeholder="Cantidad de habitaciones"
-        {...register("roomsQuantity")}
-      />
+        <input
+          type="date"
+          id="constructionDate"
+          placeholder="Fecha de construccion"
+          {...register("constructionDate")}
+        />
 
-      <input
-        type="number"
-        id="bathroomsQuantity"
-        placeholder="Cantidad de baños"
-        {...register("bathroomsQuantity")}
-      />
+        <input
+          type="text"
+          id="style"
+          placeholder="Estilo"
+          {...register("style")}
+        />
 
-      <input
-        type="number"
-        id="expensesPrice"
-        placeholder="Precio de expensas"
-        {...register("expensesPrice")}
-      />
+        <input
+          type="number"
+          id="ambientsQuantity"
+          placeholder="Cantidad de ambientes"
+          {...register("ambientsQuantity")}
+        />
 
-      <input type="submit" value="submit" />
-    </form>
+        <input
+          type="number"
+          id="roomsQuantity"
+          placeholder="Cantidad de habitaciones"
+          {...register("roomsQuantity")}
+        />
+
+        <input
+          type="number"
+          id="bathroomsQuantity"
+          placeholder="Cantidad de baños"
+          {...register("bathroomsQuantity")}
+        />
+
+        <input
+          type="number"
+          id="expensesPrice"
+          placeholder="Precio de expensas"
+          {...register("expensesPrice")}
+        />
+
+        <input type="submit" value="submit" />
+      </Form>
+    </div>
   );
 }
