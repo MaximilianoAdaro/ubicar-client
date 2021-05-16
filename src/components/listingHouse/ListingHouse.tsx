46bb0b5c40e7d64340225bf9151b49a6ff1bc5c6
AparicioFranco <franco.aparicio@ing.austral.edu.ar>
2021-05-16 00:36:33 -0300

    added api, works, not coded optimal

--- src/components/listingHouse/ListingHouse.tsx
+++ src/components/listingHouse/ListingHouse.tsx
@@ -5,6 +5,13 @@ import React from 'react'
 
 export function ListingHouse(props:any) {
 
+    const toLowerCaseExceptFirst = (data:string) => {
+        return data.charAt(0).toUpperCase() + data.slice(1).toLowerCase();
+    }
+    const house = props.house
+    const houseAddress = house.address
+    const address = `${houseAddress.street} ${houseAddress.number}, ${houseAddress.town.name}, ${houseAddress.town.city.name}, ${houseAddress.postalCode}`
+
   return (
         <a href='https://google.com/' className={styles.aTagHref}>
             <Row className={styles.propertyInformation}>
@@ -13,19 +20,19 @@ export function ListingHouse(props:any) {
                 </Col>
                 <Col className={styles.propertyTechnicalInformation}>
                     <Row className={styles.propertyCharacteristics}>
-                         {props.house.title}
+                         {house.title}
                     </Row>
                     <Row className={styles.propertyCharacteristics}>
-                        USD {props.house.price.toLocaleString()}
+                        USD {house.price.toLocaleString()}
                     </Row>
                     <Row className={styles.propertyCharacteristics}>
-                        {props.house.type} en {props.house.sale} - {props.house.size} m²
+                        {toLowerCaseExceptFirst(house.type)} in {toLowerCaseExceptFirst(house.condition)} - {house.squareFoot} m²
                     </Row>
                     <Row className={styles.propertyCharacteristics}>
-                        {props.house.habs} habitaciones  - {props.house.baths} baños
+                        {house.rooms} habitaciones  - {house.fullBaths} baños
                     </Row>
                     <Row className={styles.propertyCharacteristics}>
-                        {props.house.dir}
+                        {address}
                     </Row>
                 </Col>
             </Row>
--- src/routes/listingPage/ListingPage.tsx
+++ src/routes/listingPage/ListingPage.tsx
@@ -2,88 +2,19 @@ import styles from './ListingPage.module.scss'
 import { Col, Container, Row } from "react-bootstrap";
 import { ListingFilters } from "../../components/listingFilters";
 import { ListingHouse } from "../../components/listingHouse";
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 
 
-const casa = {
-    title:'Departamento en Belgrano, gran ubicacion',
-    price: 4000000,
-    dir: '2446 Highway Z, Wetzville, MO 63385',
-    type: 'Departamento',
-    habs: 4,
-    baths: 3,
-    size: 750,
-    sale: 'Venta'
-}
-const casa2 = {
-    title:'Casa en Nordelta, lote al lago',
-    price: 200000,
-    dir: '2500 Highway Z, Wetzville, MO 63385',
-    type: 'Casa',
-    habs: 5,
-    baths: 2,
-    size: 475,
-    sale: 'Alquiler'
-}
-const casa3 = {
-    title:'Casa en Pilar, con parque y parilla',
-    price: 650000,
-    dir: '1600 Calle X, Ciudad, CD 64403',
-    type: 'Casa',
-    habs: 7,
-    baths: 5,
-    size: 1000,
-    sale: 'Venta'
-}
-const casa4 = {
-    title:'Casa en Belgrano, gran ubicacion',
-    price: 650000,
-    dir: '1600 Calle X, Ciudad, CD 64403',
-    type: 'Departamento',
-    habs: 7,
-    baths: 5,
-    size: 1000,
-    sale: 'Venta'
-}
-const casa5 = {
-    title:'Casa en Belgrano, gran ubicacion',
-    price: 650000,
-    dir: '1600 Calle X, Ciudad, CD 64403',
-    type: 'Casa',
-    habs: 7,
-    baths: 5,
-    size: 1000,
-    sale: 'Venta'
-}
-const casa6 = {
-    title:'Casa en Belgrano, gran ubicacion',
-    price: 650000,
-    dir: '1600 Calle X, Ciudad, CD 64403',
-    type: 'Casa',
-    habs: 7,
-    baths: 5,
-    size: 1000,
-    sale: 'Venta'
-}
-
-const casas = [casa,casa2,casa3,casa4,casa5,casa6]
 export function ListingPage() {
 
-    useEffect(() => {
-        fetchItems();
-    },[]);
-
-    const [items, setItems] = useState<any[]>([])
-
-
-    const fetchItems = async () => {
-        const data = await fetch('https://fakestoreapi.com/products?limit=10');
-        const items = await data.json();
-        setItems(items)
+    const fetchPlanets = async () => {
+        const res = await fetch('/preview?page=0');
+        return res.json();
     }
 
-  return (
+    const { data, status } = useQuery("planets", fetchPlanets)
 
+  return (
     <Container className={styles.listingPageContainer}>
         <ListingFilters/>
         <Row>
@@ -91,17 +22,18 @@ export function ListingPage() {
                 Aca va el mapa
             </Col>
             <Col className={styles.listingPageCol} md={4}>
-                {
-                    casas.map(casa => (
-                        <ListingHouse house={casa}/>
+                {/*{status == 'loading' && (*/}
+                {/*    <h1>isLoading</h1>*/}
+                {/*)}*/}
+                {/*{status =='error' && (*/}
+                {/*    <h1>Error</h1>*/}
+                {/*)}*/}
+                {status == 'success' && (
+                    data.content.map((casa: any) => (
+                            <ListingHouse house={casa}/>
                         )
                     )
-                }
-                {/*{items.map(item => (*/}
-                {/*    <h2>*/}
-                {/*        {item.id}. {item.title}*/}
-                {/*    </h2>)*/}
-                {/*)}*/}
+                )}
             </Col>
         </Row>
     </Container>
