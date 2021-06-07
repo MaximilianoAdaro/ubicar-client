234a4a03a17bdbd1a594c79abe676d6b1aa459bd
AparicioFranco <franco.aparicio@ing.austral.edu.ar>
2021-06-07 16:10:31 -0300

    Changes to favorites

--- src/components/addFavorite/addFavorite.tsx
+++ src/components/addFavorite/addFavorite.tsx
@@ -1,20 +1,33 @@
+import styles from "./addFavorite.module.scss";
 import { Button, Grid } from "@material-ui/core";
 import axios from "axios";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import StarIcon from "@material-ui/icons/Star";
+import { useQuery } from "react-query";
 
 const propertyId = 5;
-const handleClick = () => {
-  axios.put("https://localhost:8080/like/" + propertyId, propertyId);
-};
 
-const handleClick2 = () => {
-  axios.put("https://localhost:8080/dislike/" + propertyId, propertyId);
-};
+// const likeProperty = () => {
+//     axios.put("https://localhost:8080/like/" + propertyId, propertyId);
+// };
+//
+// const unlikeProperty = () => {
+//     return useQuery("unlikeProperty", async () => {
+//         const { data } = await axios.put("https://localhost:8080/dislike/" + propertyId, propertyId);
+//         return data;
+//     });
+// };
 
-export function adddFavorite() {
+export function AddFavorite() {
   return (
     <div>
       <Grid>
-        <Button onClick={handleClick} />
+        <Button>
+          <StarBorderIcon className={styles.unfavorite} />
+        </Button>
+        <Button>
+          <StarIcon className={styles.favorite} />
+        </Button>
       </Grid>
     </div>
   );
