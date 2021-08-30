07aa66290e4b4f94211568a74c9938be22910e87
aparusho <franco.aparicio@ing.austral.edu.ar>
2021-08-30 11:53:29 -0300

    My properties filtered

--- src/components/UserProfile/MyProperties.tsx
+++ src/components/UserProfile/MyProperties.tsx
@@ -6,6 +6,7 @@ import { useGetMyPropertiesUsingGET } from "../../api";
 
 export function MyProperties() {
   const data = useGetMyPropertiesUsingGET();
+  console.log(data);
   return (
     <div>
       <Grid className={styles.personalDataMainDiv}>
@@ -13,10 +14,22 @@ export function MyProperties() {
         <p>Aqui puedes ver las propiedades que publicaste.</p>
       </Grid>
       <Grid className={styles.properties}>
+        <h3>Propiedades Publicadas</h3>
         {data.status === "success" &&
-          data?.data?.map((casa) => (
-            <PropertyList key={casa.id} house={casa} from={"properties"} />
-          ))}
+          data?.data
+            .filter((casa) => casa.step === 7)
+            .map((casa) => (
+              <PropertyList key={casa.id} house={casa} from={"properties"} />
+            ))}
+      </Grid>
+      <Grid className={styles.properties}>
+        <h3>Propiedades sin publicar</h3>
+        {data.status === "success" &&
+          data?.data
+            .filter((casa) => casa.step < 7)
+            .map((casa) => (
+              <PropertyList key={casa.id} house={casa} from={"properties"} />
+            ))}
       </Grid>
     </div>
   );
