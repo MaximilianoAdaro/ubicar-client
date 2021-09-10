56d8383642c2c881346fb6e21cd92000e066cd12
aparusho <franco.aparicio@ing.austral.edu.ar>
2021-09-09 22:00:57 -0300

    New footer and navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Grid, List, ListItem, Popover } from "@material-ui/core";
+import { Button, Grid, List, ListItem, Popover } from "@material-ui/core";
 import styles from "./NavBar.module.scss";
 import { useState } from "react";
 import logo from "./Logo/Logo-Ubicar.png";
@@ -8,6 +8,7 @@ import { Link, useHistory } from "react-router-dom";
 import { urls } from "../../constants";
 import firebase from "firebase";
 import { useGetLoggedUsingGET, useLogOut } from "../../api";
+import clsx from "clsx";
 
 export function NavBar() {
   const [anchorAccount, setAnchorAccount] = useState(null);
@@ -30,90 +31,79 @@ export function NavBar() {
   };
 
   return (
-    <div className={styles.divNavBarContainer}>
-      <Grid container className={styles.navBarContainer}>
-        <Grid item xs>
-          <Grid item xs={4} className={styles.navBarLogo} container>
-            <Image src={logo} alt="Logo Ubicar" className={styles.imageLogo} />
-            <p>
-              <Link to={urls.home} className={styles.UbicarTitle}>
-                Ubicar
-              </Link>
-            </p>
+    <div>
+      <Grid container className={styles.navbar_container}>
+        <Grid container xs>
+          <Grid xs>
+            <Link to={urls.listingPage} className={styles.navbar_links}>
+              Ver Publicaciones
+            </Link>
           </Grid>
-          <Grid item xs></Grid>
+          <Grid xs>
+            <Link to={urls.createProperty} className={styles.navbar_links}>
+              Publicar
+            </Link>
+          </Grid>
         </Grid>
-        <Grid item xs>
-          <Grid container className={styles.gridTagsContainer}>
-            <Grid item xs></Grid>
-            {/* <Grid item xs={3} xl={2} className={styles.gridTags}>
-              <Link to={urls.listingPage} className={styles.navBarTags}>
-                Compra
-              </Link>
-            </Grid> */}
-            <Grid item xs={3} xl={2} className={styles.gridTags}>
-              <Link to={urls.listingPage} className={styles.navBarTags}>
-                Ver Publicaciones
-              </Link>
-            </Grid>
-            <Grid item xs={3} xl={2} className={styles.gridTags}>
-              <Link to={urls.createProperty} className={styles.navBarTags}>
-                Publicar
-              </Link>
-            </Grid>
-            <Grid item xs={3} xl={2} className={styles.navBarAccount}>
+        <Grid xs className={styles.navbar_logo}>
+          <Link
+            to={urls.home}
+            className={clsx(styles.navbar_links, styles.navbar_title)}
+          >
+            Ubicar
+          </Link>
+        </Grid>
+        <Grid xs className={styles.navbar_user_dropdown}>
+          <div>
+            {user ? (
               <div>
-                {user ? (
-                  <div>
-                    <button
-                      className={styles.myAccountButton}
-                      id="buttonForm"
-                      onClick={openAccountPopover}
+                <Button
+                  className={styles.navbar_user_dropdown_button}
+                  id="buttonForm"
+                  onClick={openAccountPopover}
+                >
+                  {user.userName}
+                  <IoMdArrowDropdown
+                    className={styles.navbar_user_dropdown_icon}
+                  />
+                </Button>
+                <Popover
+                  open={Boolean(anchorAccount)}
+                  anchorEl={anchorAccount}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "center",
+                  }}
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "center",
+                  }}
+                  onClose={() => setAnchorAccount(null)}
+                >
+                  <List>
+                    <ListItem>
+                      <Link
+                        to={urls.userProfile}
+                        className={styles.myAccountDropdownAccount}
+                      >
+                        Mi perfil
+                      </Link>
+                    </ListItem>
+                    <ListItem
+                      onClick={handleLogout}
+                      className={styles.myAccountDropdownAccount}
                     >
-                      {user.userName}
-                      <IoMdArrowDropdown
-                        className={styles.myAccountDropdownIcon}
-                      />
-                    </button>
-                    <Popover
-                      open={Boolean(anchorAccount)}
-                      anchorEl={anchorAccount}
-                      anchorOrigin={{
-                        vertical: "bottom",
-                        horizontal: "center",
-                      }}
-                      transformOrigin={{
-                        vertical: "top",
-                        horizontal: "center",
-                      }}
-                      onClose={() => setAnchorAccount(null)}
-                    >
-                      <List>
-                        <ListItem>
-                          <Link
-                            to={urls.userProfile}
-                            className={styles.myAccountDropdownAccount}
-                          >
-                            Mi perfil
-                          </Link>
-                        </ListItem>
-                        <ListItem
-                          onClick={handleLogout}
-                          className={styles.myAccountDropdownAccount}
-                        >
-                          Cerrar sesión
-                        </ListItem>
-                      </List>
-                    </Popover>
-                  </div>
-                ) : (
-                  <Link to={urls.logIn} className={styles.navBarTags}>
-                    Iniciar Sesion
-                  </Link>
-                )}
+                      Cerrar sesión
+                    </ListItem>
+                  </List>
+                </Popover>
               </div>
-            </Grid>
-          </Grid>
+            ) : (
+              <Link to={urls.logIn} className={styles.navbar_links}>
+                Iniciar Sesion
+              </Link>
+            )}
+          </div>
         </Grid>
       </Grid>
     </div>
