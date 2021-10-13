17960c1ee482a54c1e8fbc58341e9cb2dbe1187d
aparusho <franco.aparicio@ing.austral.edu.ar>
2021-10-13 20:17:30 +0200

    My properties and my favorites in userProfile

--- src/components/UserProfile/MyProperties.tsx
+++ src/components/UserProfile/MyProperties.tsx
@@ -3,62 +3,68 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import { PropertyList } from "./PropertyList";
 import { useGetMyPropertiesUsingGET } from "../../api";
+import { PropertyCardMyProperties } from "./PropertyCardMyProperties";
 
 export function MyProperties() {
   const data = useGetMyPropertiesUsingGET();
   console.log(data);
   return (
-    <div>
-      <Grid className={styles.personalDataMainDiv}>
-        <h1>Mis Propiedades</h1>
+    <div className={styles.personalDataMainDiv}>
+      <Grid>
         <p>
           Aqui puedes ver las propiedades que publicaste y las que todavia no
           terminaste de publicar.
         </p>
       </Grid>
-      <Grid className={styles.properties}>
-        {data.status === "success" &&
-        data?.data.filter((casa) => casa.step === 7).length > 0 ? (
-          <div>
-            <h3>Propiedades publicadas</h3>
-            {data?.data
-              .filter((casa) => casa.step === 7)
-              .map((casa) => (
-                <PropertyList
-                  key={casa.id}
-                  house={casa}
-                  from={"properties"}
-                  state={""}
-                />
-              ))}
-          </div>
-        ) : (
-          <h3>No tienes propiedades publicadas.</h3>
-        )}
-      </Grid>
+      <Grid container>
+        <Grid className={styles.properties} xs>
+          {data.status === "success" &&
+          data?.data.filter((casa) => casa.step === 7).length > 0 ? (
+            <div>
+              <h3>Propiedades publicadas</h3>
+              <Grid className={styles.published_property_list}>
+                {data?.data
+                  .filter((casa) => casa.step === 7)
+                  .map((casa) => (
+                    <PropertyCardMyProperties
+                      key={casa.id}
+                      house={casa}
+                      from={"properties"}
+                      state={""}
+                    />
+                  ))}
+              </Grid>
+            </div>
+          ) : (
+            <h3>No tienes propiedades publicadas.</h3>
+          )}
+        </Grid>
 
-      <Grid className={styles.properties}>
-        {data.status === "success" &&
-        data?.data.filter((casa) => casa.step < 7).length > 0 ? (
-          <div>
-            <h3>Propiedades sin publicar</h3>
-            {data?.data
-              .filter((casa) => casa.step < 7)
-              .map((casa) => (
-                <PropertyList
-                  key={casa.id}
-                  house={casa}
-                  from={"properties"}
-                  state={"notfinished"}
-                />
-              ))}
-          </div>
-        ) : (
-          <div></div>
-        )}
-        <br />
-        <br />
-        <br />
+        <Grid className={styles.properties} xs>
+          {data.status === "success" &&
+          data?.data.filter((casa) => casa.step < 7).length > 0 ? (
+            <div>
+              <h3>Propiedades sin publicar</h3>
+              <Grid className={styles.published_property_list}>
+                {data?.data
+                  .filter((casa) => casa.step < 7)
+                  .map((casa) => (
+                    <PropertyCardMyProperties
+                      key={casa.id}
+                      house={casa}
+                      from={"properties"}
+                      state={"notfinished"}
+                    />
+                  ))}
+              </Grid>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <br />
+          <br />
+          <br />
+        </Grid>
       </Grid>
     </div>
   );
