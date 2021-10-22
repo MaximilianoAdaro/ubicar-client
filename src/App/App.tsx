e3f19de2e399b5a4f47f66ef70cd123609c02493
katiacammisa <katia.cammisa@ing.austral.edu.ar>
2021-10-22 12:01:13 +0200

    New home page

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -15,18 +15,14 @@ import {
 } from "../routes";
 import { actions, useAppDispatch, useAppSelector } from "../store";
 import { selectRedirectPath } from "../store/slices/session";
-import styles from "./App.module.scss";
 import { Loading } from "../components/common/loading/Loading";
 import { EditProperty } from "../routes/editProperty";
 import { Footer } from "../components/footer/Footer";
 import { useGetLoggedUsingGET } from "../api";
 import { useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { CircularProgress, TextField } from "@material-ui/core";
-import React, { useState } from "react";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import { useHistory } from "react-router-dom";
-import { convertCoordinates } from "../components/Map/utils";
+import React from "react";
+import { Home } from "../routes/home/Home";
 
 export default function App() {
   const redirectPath = useAppSelector(selectRedirectPath);
@@ -65,7 +61,7 @@ export default function App() {
         }}
       >
         <Switch>
-          <Route exact path={urls.home} component={WorkInProgress} />
+          <Route exact path={urls.home} component={Home} />
           <ProtectedRoute
             {...defaultProtectedRouteProps}
             exact
@@ -86,127 +82,3 @@ export default function App() {
     </>
   );
 }
-
-const WorkInProgress = () => {
-  const { data: user } = useGetLoggedUsingGET();
-  const dispatch = useAppDispatch();
-  const history = useHistory();
-
-  const [open, setOpen] = useState(false);
-  const [options, setOptions] = useState([
-    { id: "", name: "", stateName: "", centroid: { lat: 0, long: 0 } },
-  ]);
-  const loading = open && options.length === 0;
-
-  const onChangeHandle = async (value: any) => {
-    const response = await fetch(
-      "http://localhost:3000/public/cities-and-states?name=" + value
-    );
-
-    const citiesAndStates = await response.json();
-    if (citiesAndStates && citiesAndStates.content) {
-      const options = citiesAndStates.content.map((option: any) => {
-        if (option.type === "STATE") {
-          return option.state;
-        } else {
-          return option.city;
-        }
-      });
-      setOptions(options);
-    }
-  };
-
-  const handleChangeView = (longitude: number, latitude: number) => {
-    dispatch(actions.map.setView({ longitude: longitude, latitude: latitude }));
-    dispatch(actions.map.setZoom(12));
-  };
-  const handleChangeName = (name: string) => {
-    dispatch(actions.session.setSearchBar(name));
-  };
-
-  const handleOptionLabel = (option: any) => {
-    if (option.id !== "") {
-      if (option.stateName)
-        return (
-          option.name[0].toUpperCase() +
-          option.name.substr(1).toLowerCase() +
-          ", " +
-          option.stateName
-        );
-      return option.name[0].toUpperCase() + option.name.substr(1).toLowerCase();
-    }
-    return "";
-  };
-
-  const handleOption = (value: any) => {
-    dispatch(actions.session.setOption(value));
-    history.push("/listing-page");
-  };
-
-  return (
-    <div className={styles.app}>
-      <div
-        style={{
-          marginBottom: 20,
-        }}
-      >
-        {user && <h4>Bienvenido {user.userName}</h4>}
-
-        <div style={{ margin: "auto" }}>
-          <Autocomplete
-            id="asyncState"
-            style={{ width: "500px" }}
-            open={open}
-            onChange={(e, value) => {
-              if (value) {
-                let coords = convertCoordinates(
-                  value.centroid.long,
-                  value.centroid.lat
-                );
-                handleChangeName(handleOptionLabel(value));
-                handleChangeView(coords[0], coords[1]);
-                handleOption(value);
-              }
-            }}
-            onOpen={() => {
-              setOpen(true);
-            }}
-            onClose={() => {
-              setOpen(false);
-            }}
-            getOptionSelected={(option, value) => option.name === value.name}
-            getOptionLabel={(option) => handleOptionLabel(option)}
-            options={options}
-            loading={loading}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                fullWidth
-                label={"Introduzca una dirección!"}
-                variant="outlined"
-                color={"secondary"}
-                onChange={(ev) => {
-                  // dont fire API if the user delete or not entered anything
-                  if (ev.target.value !== "" || ev.target.value !== null) {
-                    onChangeHandle(ev.target.value);
-                  }
-                }}
-                InputProps={{
-                  ...params.InputProps,
-                  endAdornment: (
-                    <React.Fragment>
-                      {loading ? (
-                        <CircularProgress color="inherit" size={20} />
-                      ) : null}
-                      {params.InputProps.endAdornment}
-                    </React.Fragment>
-                  ),
-                }}
-              />
-            )}
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
